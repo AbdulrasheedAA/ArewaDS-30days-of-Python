dc43694c2fd370ec2390c2472149587f508900fc
AbdulraheedAA <abdulrasheed.abdulraheem.a@gmail.com>
2023-11-29 07:02:13 +0100

    Day 1 assignment-Basic python syntax

---
+++ day1/helloworld.py
@@ -0,0 +1,18 @@
+# Simple mathematical operations
+print(14 + 20)             # addition(+)
+print(100 - 120)             # subtraction(-)
+print(4 * 2.5)             # multiplication(*)
+print(1000 / 25)             # division(/)
+print(4 ** 3)            # exponential(**)
+print(5 % 3)             # modulus(%)
+print(11 // 4)            # Floor division operator(//)
+
+# data types
+print(type(140))          # Int
+print(type(75.82))        # Float
+print(type(4 + 5j))      # Complex number
+print(type('Abdulrasheed'))  # String
+print(type([5, 6, 7]))   # List
+print(type({'name':'Abdulrasheed'})) # Dictionary
+print(type({5.4, 7.8, 11.6}))    # Set
+print(type((5.4, 7.8, 11.6)))    # Tuple
