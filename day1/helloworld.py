69dd7e17907d85d9bd9bf5fd4eea8b769a8308f6
AbdulraheedAA <abdulrasheed.abdulraheem.a@gmail.com>
2023-11-29 18:56:14 +0100

    Day 1 assignment-Basic python syntax

--- day1/helloworld.py
+++ day1/helloworld.py
@@ -1,4 +1,17 @@
-# Simple mathematical operations
+print("")
+print("")
+
+#1. Check Python Version
+print('1. Check Python Version:-')
+import sys
+
+print("Python version", sys.version)
+print("")
+print("")
+
+# 2. Simple mathematical operations
+print('2. Simple mathematical operations:-')
+
 print(14 + 20)             # addition(+)
 print(100 - 120)             # subtraction(-)
 print(4 * 2.5)             # multiplication(*)
@@ -7,12 +20,29 @@ print(4 ** 3)            # exponential(**)
 print(5 % 3)             # modulus(%)
 print(11 // 4)            # Floor division operator(//)
 
-# data types
-print(type(140))          # Int
-print(type(75.82))        # Float
-print(type(4 + 5j))      # Complex number
-print(type('Abdulrasheed'))  # String
-print(type([5, 6, 7]))   # List
-print(type({'name':'Abdulrasheed'})) # Dictionary
-print(type({5.4, 7.8, 11.6}))    # Set
-print(type((5.4, 7.8, 11.6)))    # Tuple
+print("")
+print("")
+
+#3. Write strings
+print('3. Write strings:-')
+
+name = 'Abdulrasheed'
+surname = 'Abdulraheem'
+country = 'Nigeria'
+message = 'I am enjoying 30 days of python'
+print('My named is',  name,  surname, 'I am from',  country,  message)
+
+print("")
+print("")
+
+# 4. Checking data types
+print('4. Checking data types:-')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(surname))
+print(type(country))
